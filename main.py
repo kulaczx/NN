7cc20e412d090aa0e3bf209685e8693aeb35f41f
kulaczx <yli607@ucr.edu>
2021-12-08 00:10:49 -0800

    tested forward and backward

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from nearestN import NearestNeighbor
 
 
 def menu():
+    NN = NearestNeighbor()
     file_name = ''
     alg_num = 0
     print("Welcome to Bertie Woosters Feature Selection Algorithm")
     print("Type in the name of the file to test: ", end='')
     input(file_name)
-
+    NN.read_data(file_name)
     print("Type the number of the algorithm you want to run.")
     print("1) Forward Selection")
     print("2) Backward Elimination")
@@ -17,11 +18,25 @@ def menu():
 
     if alg_num == 1:
         # run forward
-        print("run forward")
+        timer = time.time()
+        result = NN.forward_selection()
+        endTime = time.time()
+        total_time = endTime - timer
+        print("Result of Forward Selection is: ", result, " eclipse time: ", total_time)
     if alg_num == 2:
         # run backward
-        print("run backward")
+        timer = time.time()
+        result = NN.backward_elimination()
+        endTime = time.time()
+        total_time = endTime - timer
+        print("Result of Backward Elimination is: ", result, " eclipse time: ", total_time)
 
+
+large_data = "Ver_2_CS170_Fall_2021_LARGE_data__82.txt"
+small_data = "Ver_2_CS170_Fall_2021_Small_data__43.txt"
 nn = NearestNeighbor()
-nn.read_data("Ver_2_CS170_Fall_2021_Small_data__43.txt")
+nn.read_data(small_data)
 nn.print_data()
+#f = nn.forward_selection()
+#print(f)
+b = nn.backward_elimination()
--- nearestN.py
+++ nearestN.py
@@ -9,13 +9,22 @@ def distance(x, y, p=2):
     for i in range(size):
         sum_x += pow((x[i] - y[i]), p)
 
-    return pow(sum_x, 1 / p)
+    return np.sqrt(sum_x)
+
+
+def make_feature(feature, remove_number):
+    new_feature = []
+    for i in feature:
+        if i != remove_number:
+            new_feature.append(i)
+    return new_feature
 
 
 class NearestNeighbor:
     def __init__(self):
         self.data_list = []
         self.data_label = []
+        self.unique_label = [1.0, 2.0]
 
     def print_data(self):
         for i in range(len(self.data_list)):
@@ -43,7 +52,8 @@ class NearestNeighbor:
         for i in range(len(self.data_list)):
             training_data = self.make_test_data(feature, i)
             dist = distance(test_data, training_data)
-            distance_list.append(training_data, dist, self.data_label[i])
+            #print("test data: ", test_data, "target: ", training_data ,"dist: ",dist)
+            distance_list.append((training_data, dist, self.data_label[i]))
 
         distance_list.sort(key=lambda tup: tup[1])
         neighbors_label = list()
@@ -51,11 +61,11 @@ class NearestNeighbor:
             neighbors.append(distance_list[i][0])
             neighbors_label.append(distance_list[i][2])
 
-        unique_label_ = np.unique(np.array(self.data_label))
-        label_counts = np.zeros(len(unique_label_))
+        label_counts = np.zeros(2)
         for i in range(K):
-            for j in len(unique_label_):
-                if neighbors_label[i] == unique_label_[j]:
+            for j in range(len(self.unique_label)):
+                #print(neighbors_label[i], self.unique_label[j])
+                if neighbors_label[i] == self.unique_label[j]:
                     label_counts[j] += 1
         index = 0
         prev = label_counts[0]
@@ -64,13 +74,14 @@ class NearestNeighbor:
             if label_counts[i] > prev:
                 index = i
                 prev = label_counts[i]
-
-        return unique_label_[index]
+        #print("return ", self.unique_label[index])
+        return self.unique_label[index]
 
     def accuracy(self, test_labels):
         number_of_data = len(self.data_list)
         correct_count = 0
-        for i in number_of_data:
+        for i in range(number_of_data):
+            #print("test label: ", test_labels[i], "target label: ", self.data_label[i])
             if test_labels[i] == self.data_label[i]:
                 correct_count += 1
 
@@ -80,9 +91,10 @@ class NearestNeighbor:
         test_data = []
         for i in lst:
             test_data.append(self.data_list[index][i])
+        #print(test_data)
         return test_data
 
-    def forward_selection(self, sLevel=0.5):
+    def forward_selection(self, sLevel=0.05):
         best_feature = []
         feature_number = len(self.data_list[0])
         init_feature = []
@@ -96,34 +108,80 @@ class NearestNeighbor:
             acc_list.clear()
             if len(best_feature) == 0:
                 for i in init_feature:
+                    feature = list([i])
+                    print("Feature: ", feature, end='')
+                    test_label.clear()
                     for j in range(len(self.data_list)):
+                        test_data.clear()
                         test_data.append(self.data_list[j][i])
-                        feature = [i]
-                        test_label.append(self.KNN(test_data, feature))
+                        test_label.append(self.KNN(test_data, feature, K=5))
                     acc = (i, self.accuracy(test_label))
+                    print(" Accuracy: ", acc[1])
                     acc_list.append(acc)
                 acc_list.sort(reverse=True, key=lambda pair: pair[1])
             else:
-                for i in range(feature_number):
+                print("best feature not empty")
+                feature = []
+                for i in init_feature:
+                    feature.clear()
+                    feature = list(best_feature)
+                    feature.append(i)
+                    print("Feature: ", feature)
+                    test_label.clear()
                     for j in range(len(self.data_list)):
-                        feature = best_feature
-                        feature.append(i)
+                        test_data.clear()
                         test_data = self.make_test_data(best_feature, j)
                         test_data.append(self.data_list[j][i])
-                        test_label.append(self.KNN(test_data, feature))
+                        #print(test_data)
+                        test_label.append(self.KNN(test_data, feature, K=5))
                     acc = (i, self.accuracy(test_label))
+                    print(" Accuracy: ", acc[1])
                     acc_list.append(acc)
                 acc_list.sort(reverse=True, key=lambda pair: pair[1])
 
             best_feature.append(acc_list[0][0])
             init_feature.remove(acc_list[0][0])
-            if 1 - acc_list[0][1] < sLevel:
+            if (1 - acc_list[0][1]) < sLevel:
+                print(1 - acc_list[0][1], "less than sLevel")
                 break
 
         return best_feature
 
-    def backward_elimination(self, target, sLevel=0.5):
-        best_feature = []
-        init_feature = self.data_list
+    def backward_elimination(self, sLevel=0.05):
+        feature_number = len(self.data_list[0])
+        feature = []
+        for i in range(feature_number):
+            feature.append(i)
+        test_label = []
+        acc_list = []
+        test_data = []
+        next_feature = []
+        for i in self.data_list:
+            test_label.append(self.KNN(i, feature, K=5))
+        acc_all = self.accuracy(test_label)
+        if 1 - acc_all < sLevel:
+            return feature
+        while len(feature) > 0:
+            acc_list.clear()
+            for i in feature:
+                test_label.clear()
+                next_feature.clear()
+                next_feature = make_feature(feature, i)
+                print("Feature: ", next_feature)
+                for j in range(len(self.data_list)):
+                    test_data.clear()
+                    test_data = self.make_test_data(next_feature, j)
+                    test_label.append(self.KNN(test_data, next_feature, K=5))
+                acc = (i, self.accuracy(test_label))
+                print("acc: ", acc)
+                print(" Accuracy: ", acc[1])
+                acc_list.append(acc)
+            acc_list.sort(reverse=True, key=lambda pair: pair[1])
+            print("remaining feature: ", feature)
+            print("remove: ", acc_list[0][0])
+            feature.remove(acc_list[0][0])
+            if (1 - acc_list[0][1]) < sLevel:
+                print(1 - acc_list[0][1], "less than sLevel")
+                break
 
-        return best_feature
+        return feature
